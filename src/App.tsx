f73d5ee34b43c4d2e23d8cfcd2ce983f91c7aff4
pierrechannel <pierrechannel0428@gmail.com>
2026-01-09 08:41:20 +0200

    Updated Changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Gallery from "./pages/Gallery";
-import Rooms from "./pages/Rooms";
-import Restaurant from "./pages/Restaurant";
-import RoomDetails from "./pages/RoomDetails";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -23,11 +18,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/a-propos" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/galerie" element={<Gallery />} />
-          <Route path="/chambres" element={<Rooms />} />
-          <Route path="/chambres/:slug" element={<RoomDetails />} />
-          <Route path="/restaurant" element={<Restaurant />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
