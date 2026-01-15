91ae4353c36f0900204dbcbf4e03f0bf4951b0eb
pierrechannel <pierrechannel0428@gmail.com>
2026-01-15 17:52:55 +0200

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'ruvubu-app',
+    script: 'node_modules/.bin/vite',
+    args: 'preview --host --port 3000',
+    cwd: '/var/www/html/ruvubu',
+    interpreter: 'node',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+}
